a9aa29eff634b02e1077f5e7d43f5cfbd4aa9106
raj1008-git <macof.rj@gmail.com>
2026-01-04 16:24:25 +0545

    Learned using initial 5 questions

---
+++ Chapter 1/04_listdir.py
@@ -0,0 +1,5 @@
+import os # Import the OS module to interact with the Operating System
+
+# The listdir() function returns a list of all files and folders in the current directory
+# We print this list to the console
+print(os.listdir())
