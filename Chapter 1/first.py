c7f2be9b1ea8daf13aff96b5e59078c2c2b36a53
raj1008-git <macof.rj@gmail.com>
2026-01-04 15:56:45 +0545

    Learned about modules and REPL

--- Chapter 1/first.py
+++ Chapter 1/first.py
@@ -1,6 +1,3 @@
-# This is a comment: The line below prints text to the screen
-print("Hello World")
-
-import os # Importing a built-in module
-# The module does the hard work of finding the path, but YOU write the code to ask for it
-print("My current folder is:", os.getcwd()) # The () are required to RUN the function
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
