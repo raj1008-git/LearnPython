3bed94a1011c877fb2fe48aa131dbe1f4b644df0
raj1008-git <macof.rj@gmail.com>
2026-03-06 14:22:01 +0545

    Variables and Data Types

--- PythonFundamentals/hello.py
+++ PythonFundamentals/hello.py
@@ -1,5 +1,17 @@
-x=10
-x="hello"
-x=3.14
-print(x)
-print(type(x))
+name = "Arjun"
+city= "Kathmandu"
+
+age= 25
+year = 2026
+
+
+height=5.9
+price= 19.99
+
+is_student=True
+has_job=False
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
